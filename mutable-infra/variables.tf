a56649e9f7fa21eff7afd4303299ed06e7262d49
vinod sjml <vinsjml@gmail.com>
2024-03-11 09:53:19 +0530

    push

---
+++ mutable-infra/variables.tf
@@ -0,0 +1,9 @@
+variable "ENV" {}
+variable "SPOT_INSTANCE_COUNT" {}
+variable "SPOT_INSTANCE_TYPE" {}
+variable "OD_INSTANCE_TYPE" {}
+variable "OD_INSTANCE_COUNT" {}
+variable "APP_PORT" {}
+variable "COMPONENT" {}
+variable "INTERNAL" {}
+variable "APP_VERSION" {}
